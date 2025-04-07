2e1d65236f17311c1fcf2d7a7cf574d6e7d1d1ed
QuentinBonzom <159001480+QuentinBonzom@users.noreply.github.com>
2025-04-07 17:27:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import React from "react";
 
 import Description from './components/description';
 import Header from "./components/header";
+import Footer from "./components/footer
 
 function App() {
   return (
     <>
     <Description />
       <Header />
+      <Footer/>
 
     </>
   );
